6edda8ccda2e1632e1a9f42936d82486d65f2c52
Monk <aadityaura@gmail.com>
2022-12-29 19:16:02 +0530

    Update prompt_utils.py

--- promptify/prompts/nlp/prompt_utils.py
+++ promptify/prompts/nlp/prompt_utils.py
@@ -2,18 +2,19 @@ import json
 import numpy as np
 
 def read_json(file):
-    with open(file, "r") as f:
+    with open(file, 'r') as f:
         data = json.load(f)
     return data
 
-
 def get_examples(n, task, domain):
-    task_dict = {"ner": "ner.json"}
+    
+    task_dict = {'ner': 'ner.json', 'multilabel': 'ml.json'}
     if task in task_dict:
-        examples = read_json(task_dict[task])
-        examples = [k for k in examples if k["domain"] == domain]
-        examples = np.random.choice(examples, n)
-        examples = [{"sentence": k["sentence"], "tags": k["tags"]} for k in examples]
+        examples  = read_json(task_dict[task])
+        examples  = [k for k in examples if k['domain']==domain]
+        examples  = np.random.choice(examples, n)
+        
+        examples  = [{"sentence": k['sentence'], "data": k["data"]} for k in examples]
         return examples
       
       
