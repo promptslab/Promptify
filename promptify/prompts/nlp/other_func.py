aa57bd3ebd5f840cb61232973e06b36653ac6bd3
Monk <aadityaura@gmail.com>
2023-01-01 05:11:39 +0530

    Update other_func.py

--- promptify/prompts/nlp/other_func.py
+++ promptify/prompts/nlp/other_func.py
@@ -1,64 +1,58 @@
-def binary_classification(text_input, config=None):
-    default_config = {
-        "description": "",
-        "examples": [],
-        "labels"  : [],
-    }
-
-    if config:
-        default_config.update(config)
-
-    assert (
-        default_config["labels"] != []
-    ), "Please provide Binary Labels as a list -> ['Positive', 'Negative']"
-    labels = default_config["labels"]
-    description = default_config["description"]
-
-    if description != "":
-        template = (
-            description
-            + f"\nPerform Binary Text Classification, the output will be either {labels[0]} or {labels[1]}\n\n"
-        )
+def binary_classification(text_input, labels, description="", examples=[]):
+    """
+    Perform Binary Text Classification, the output will be either the first label or the second label in the provided list.
+    
+    Parameters:
+    text_input (str): The input text to be classified.
+    labels (list): A list of two possible labels for the classification.
+    description (str, optional): A description of the classification task. Default is an empty string.
+    examples (list, optional): A list of examples for the classification task. Default is an empty list.
+    
+    Returns:
+    str: A string template describing the classification task and the input and output of the function.
+    """
+    # create the template string with the labels
+    if description:
+        template = f"{description}\nPerform Binary Text Classification, the output will be either {labels[0]} or {labels[1]}\n\n"
     else:
         template = f"Perform Binary Text Classification, the output will be either {labels[0]} or {labels[1]}\n\n"
 
-    if default_config["examples"]:
-        examp = format_examples(default_config["examples"])
+    # if examples are provided, add them to the template
+    if examples:
+        examp = format_examples(examples)
         template = template + examp
 
+    # add the input text to the template
     template = template + f"[P]:{text_input}\n[O]:"
     return template
 
 
 
+def multiclass_classification(text_input, labels, description="", examples=[]):
+    """
+    Perform Multiclass Text Classification, the output class must be from provided Label List.
+    
+    Parameters:
+    text_input (str): The input text to be classified.
+    labels (list): A list of possible labels for the classification.
+    description (str, optional): A description of the classification task. Default is an empty string.
+    examples (list, optional): A list of examples for the classification task. Default is an empty list.
+    
+    Returns:
+    str: A string template describing the classification task and the input and output of the function.
+    """
+    # create the template string with the list of labels
+    template = f"Perform Multiclass Text Classification, the output class must be from provided Label List\n[Label List]: {labels}\n\n"
 
-def multiclass_classification(text_input, config=None):
-    default_config = {
-        "description": "",
-        "examples": [],
-        "labels": [],
-    }
-
-    if config:
-        default_config.update(config)
-
-    assert (
-        default_config["labels"] != []
-    ), "Please provide Labels as a list -> ['Positive', 'Negative', 'Mixed', ... n labels]"
-    labels = default_config["labels"]
-    description = default_config["description"]
-
-    if description != "":
-        template = (
-            description
-            + f"\nPerform Multiclass Text Classification, the output class must be from provided Label List\n[Label List]: {default_config['labels']}\n\n"
-        )
-    else:
-        template = f"Perform Multiclass Text Classification, the output class must be from provided Label List\n[Label List]: {default_config['labels']}\n\n"
-
-    if default_config["examples"]:
-        examp = format_examples(default_config["examples"])
+    # if examples are provided, add them to the template
+    if examples:
+        examp = format_examples(examples)
         template = template + examp
 
+    # if a description is provided, add it to the beginning of the template
+    if description:
+        template = f"{description}\n{template}"
+
+    # add the input text to the template
     template = template + f"[P]:{text_input}\n[O]:"
     return template
