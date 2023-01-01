248b7b84e73a4c6bfb22e5df3870017ecb5f5984
Monk <aadityaura@gmail.com>
2023-01-02 03:42:59 +0530

    Update summary.py

--- promptify/prompts/nlp/summary.py
+++ promptify/prompts/nlp/summary.py
@@ -6,11 +6,11 @@ def Summary(
         This function returns a template string with the input context. The template also includes a description of the function's purpose. If a domain is provided, it is
         included in the function's description.
         """
-    
+
         if domain:
-            template = f"You are a highly intelligent {domain} domain expert Explanation bot. You take Passage as input and explain the passage as a {domain} expert, meaningful and accurate explanation. Your output format is only [{{'E': Explained paragraph}},] form, no other form.\n"
+            template = f"You are a highly intelligent {domain} domain expert Summarization bot. You take Passage as input and summarize the passage as a {domain} expert. Your output format is only [{{'S': Summarization paragraph}},] form, no other form.\n"
         else:
-            template = "You are a highly intelligent Explanation bot. You take Passage as input and explain the passage as an expert,meaningful and accurate explanation. Your output format is only [{'E': Explained paragraph},] form, no other form.\n"
+            template = "You are a highly intelligent Summarization bot. You take Passage as input and summarize the passage as an expert. Your output format is only [{'S': Summarization paragraph},] form, no other form.\n"
 
         # add the input context and question to the template
         template += f"\nPassage: {context}\nOutput:"
