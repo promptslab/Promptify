b19ec16d6a4ce25b9675b51577eaac51222f0f8e
Monk <aadityaura@gmail.com>
2023-01-02 20:22:53 +0530

    Update summary.py

--- promptify/prompts/nlp/summary.py
+++ promptify/prompts/nlp/summary.py
@@ -1,6 +1,7 @@
 def Summary(
         context: str,  # the context paragraph
         domain: str = "",  # the domain of the question, optional
+        token_length= ""
           ):
         """
         This function returns a template string with the input context. The template also includes a description of the function's purpose. If a domain is provided, it is
@@ -8,7 +9,11 @@ def Summary(
         """
 
         if domain:
-            template = f"You are a highly intelligent {domain} domain expert Summarization system. You take Passage as input and summarize the passage as a {domain} expert. Your output format is only [{{'S': Summarization paragraph}},] form, no other form.\n"
+            if token_length!="":
+                template = f"You are a highly intelligent {domain} domain expert Summarization system. You take Passage as input and summarize the passage as a {domain} expert in {token_length} tokens. Your output format is only [{{'S': Summarization paragraph}},] form, no other form.\n"
+            else:
+                template = f"You are a highly intelligent {domain} domain expert Summarization system. You take Passage as input and summarize the passage as a {domain} expert. Your output format is only [{{'S': Summarization paragraph}},] form, no other form.\n"
+        
         else:
             template = "You are a highly intelligent Summarization system. You take Passage as input and summarize the passage as an expert. Your output format is only [{'S': Summarization paragraph},] form, no other form.\n"
 
