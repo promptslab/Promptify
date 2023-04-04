53ff5eb22fbe0f7cf5f69869a48154aff427b029
Monk <aadityaura@gmail.com>
2023-04-04 17:33:58 +0530

    Update __init__.py

--- promptify/__init__.py
+++ promptify/__init__.py
@@ -1,4 +1,7 @@
 __version__ = "0.1.4"
-from .models.nlp.hub_model import HubModel
-from .models.nlp.openai_model import OpenAI
-from .prompts.nlp.prompter import Prompter
+from .parser.parser import Parser
+from .prompter.nlp_prompter import Prompter
+from .models.nlp.text2text.openai_complete import OpenAI_Complete
+from .models.nlp.text2text.hub_model import HuggingFace
+from .models.nlp.text2text.mock_model import MockModel
+from .models.nlp.text2text.base_model import Model
