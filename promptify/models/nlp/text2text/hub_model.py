8746b14c47009c189a7147234e74f849b6b2fe5c
Monk <aadityaura@gmail.com>
2023-04-08 16:47:09 +0530

    Update hub_model.py

--- promptify/models/nlp/text2text/hub_model.py
+++ promptify/models/nlp/text2text/hub_model.py
@@ -12,6 +12,52 @@ import requests
 
 
 class HuggingFace(Model):
+
+    """
+    A class representing the HuggingFace API for text completion using various models.
+
+    Attributes
+    ----------
+    name : str
+        The name of the model, set to "HuggingFace".
+    description : str
+        A short description of the model, set to "HuggingFace API for text completion using various models".
+
+    Methods
+    -------
+    __init__(self, api_key=None, model_id_or_url="google/flan-t5-xl", wait_for_model=True, use_cache=True, top_k=None,
+            top_p=None, temperature=1.0, repetition_penalty=None, max_new_tokens=None, max_time=None,
+            num_return_sequences=1, do_sample=True, api_wait=None, api_retry=None, max_completion_length=20):
+        Initializes a new HuggingFace object with the specified parameters.
+
+    supported_models() -> Dict[str, str]:
+        Returns a dictionary of all the models supported by HuggingFace.
+
+    _verify_model(self):
+        Verifies if the given model is supported by HuggingFace.
+
+    set_key(self, api_key: Optional[str]):
+        Sets the API key to be used to connect to HuggingFace.
+
+    set_model(self, model: str):
+        Sets the HuggingFace model to be used for text completion.
+
+    get_description(self) -> str:
+        Returns a short description of the HuggingFace model being used for text completion.
+
+    model_output(self, response: Dict) -> Dict:
+        Extracts the generated text from the HuggingFace model response.
+
+    get_endpoint(self) -> str:
+        Gets the API endpoint of the HuggingFace model being used for text completion.
+
+    get_parameters(self) -> Dict[str, Union[str, int, float, List[str], Dict[str, int]]]:
+        Gets the parameters used to configure the HuggingFace model for text completion.
+
+    run(self, prompts: List[str]) -> List[Optional[str]]:
+        Runs the HuggingFace model for text completion on the specified prompts and returns a list of responses.
+    """
+
     name = "HuggingFace"
     description = "HuggingFace API for text completion using various models"
 
@@ -33,6 +79,47 @@ class HuggingFace(Model):
         api_retry=None,
         max_completion_length: int = 20,
     ):
+        """
+        Initializes a new HuggingFace object with the specified parameters.
+
+        Parameters
+        ----------
+        api_key : Optional[str], default=None
+            The API key to use to connect to HuggingFace.
+        model_id_or_url : str, default="google/flan-t5-xl"
+            The ID or URL of the HuggingFace model to use for text completion.
+        wait_for_model : bool, default=True
+            Whether to wait for the HuggingFace model to be ready before sending requests to it.
+        use_cache : bool, default=True
+            Whether to use the cache when sending requests to the HuggingFace model.
+        top_k : Optional[int], default=None
+            The maximum number of top-k candidates to consider when generating text.
+        top_p : Optional[float], default=None
+            The cumulative probability of top-p candidates to consider when generating text.
+        temperature : float, default=1.0
+            The temperature to use when generating text.
+        repetition_penalty : Optional[float], default=None
+            The repetition penalty to use when generating text.
+        max_new_tokens : Optional[int], default=None
+            The maximum number of new tokens to generate when completing text.
+        max_time : Optional[float], default=None
+            The maximum amount of time to spend generating text, in seconds.
+        num_return_sequences : int, default=1
+            The number of sequences to generate for each prompt.
+        do_sample : bool, default=True
+            Whether to use sampling when generating text.
+        api_wait : None
+            Not used.
+        api_retry : None
+            Not used.
+        max_completion_length : int, default=20
+            The maximum length of the completed text.
+
+        Returns
+        -------
+        None
+        """
+
         self.set_key(api_key)
         self.set_model(model_id_or_url)
         super().__init__(self.api_key, self.model, api_wait, api_retry)
@@ -54,6 +141,15 @@ class HuggingFace(Model):
 
     @classmethod
     def supported_models(cls) -> Dict[str, str]:
+        """
+        Returns a dictionary of all the models supported by HuggingFace.
+
+        Returns
+        -------
+        Dict[str, str]
+            A dictionary where the keys are the model IDs and the values are short descriptions of the models.
+        """
+
         response = requests.get(
             "https://huggingface.co/api/models?pipeline_tag=text2text-generation&sort=downloads&direction=-1"
         )
@@ -65,16 +161,55 @@ class HuggingFace(Model):
         }
 
     def _verify_model(self):
+        """
+        Verifies if the given model is supported by HuggingFace.
+
+        Raises
+        ------
+        ValueError
+            If the model is not supported by HuggingFace.
+
+        Returns
+        -------
+        None
+        """
+
         if self.model not in self.supported_models():
             raise ValueError(f"Unsupported model: {self.model}")
 
     def set_key(self, api_key: Optional[str]):
+        """
+        Sets the API key to be used to connect to HuggingFace.
+
+        Parameters
+        ----------
+        api_key : Optional[str]
+            The API key to use to connect to HuggingFace.
+
+        Returns
+        -------
+        None
+        """
+
         self.api_key = api_key
         self._headers = build_hf_headers(
             token=api_key, library_name="promptify", library_version=__version__
         )
 
     def set_model(self, model: str):
+        """
+        Sets the HuggingFace model to be used for text completion.
+
+        Parameters
+        ----------
+        model : str
+            The ID or URL of the HuggingFace model to use for text completion.
+
+        Returns
+        -------
+        None
+        """
+
         self.model = model
         if self.model.startswith("https://") and "huggingface" in self.model:
             # User provided a URL pointing to a Inference Endpoint
@@ -84,9 +219,42 @@ class HuggingFace(Model):
             self._url = self.get_endpoint()
 
     def get_description(self) -> str:
+        """
+        Returns a short description of the HuggingFace model.
+
+        Returns
+        -------
+        str
+            A short description of the HuggingFace model.
+
+        Notes
+        -----
+        This function calls the `supported_models` class method to get a dictionary of all the supported models,
+        and returns the short description for the current model ID.
+        """
+
         return self.supported_models()[self.model]
 
     def model_output(self, response: Dict) -> Dict:
+        """
+        Returns the model output in a dictionary format.
+
+        Parameters
+        ----------
+        response : Dict
+            The response from the HuggingFace API.
+
+        Returns
+        -------
+        Dict
+            A dictionary containing the model output.
+
+        Notes
+        -----
+        This function converts the API response into a dictionary format, where the "text" key maps to a list of
+        strings containing the generated text for each input prompt.
+        """
+
         data = {}
         data["text"] = [
             [item["generated_text"] for item in each_result.json()]
@@ -95,6 +263,20 @@ class HuggingFace(Model):
         return data
 
     def get_endpoint(self) -> str:
+        """
+        Returns the API endpoint URL for the HuggingFace model.
+
+        Returns
+        -------
+        str
+            The API endpoint URL for the HuggingFace model.
+
+        Raises
+        ------
+        ValueError
+            If the model does not exist on the Huggingface Hub, or if the model pipeline is not supported.
+        """
+
         try:
             info = model_info(self.model, token=self.api_key)
         except RepositoryNotFoundError:
@@ -114,6 +296,15 @@ class HuggingFace(Model):
     def get_parameters(
         self,
     ) -> Dict[str, Union[str, int, float, List[str], Dict[str, int]]]:
+        """
+        Returns a dictionary of the model parameters.
+
+        Returns
+        -------
+        Dict[str, Union[str, int, float, List[str], Dict[str, int]]]
+            A dictionary containing the model parameters.
+        """
+
         return {
             "wait_for_model": self.wait_for_model,
             "use_cache": self.use_cache,
@@ -128,6 +319,25 @@ class HuggingFace(Model):
         }
 
     def run(self, prompts: List[str]) -> List[Optional[str]]:
+        """
+        Runs the HuggingFace model on the given prompts.
+
+        Parameters
+        ----------
+        prompts : List[str]
+            A list of text prompts to complete.
+
+        Returns
+        -------
+        List[Optional[str]]
+            A list of completed text strings, one for each input prompt.
+
+        Notes
+        -----
+        This function sends a POST request to the HuggingFace API for each input prompt, with the appropriate model
+        parameters and headers. It returns a list of completed text strings, one for each input prompt.
+        """
+
         result = []
         for prompt in prompts:
             response = requests.post(
