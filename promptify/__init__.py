c7e184699e189052565d9602998360e0c720921c
Monk <aadityaura@gmail.com>
2023-04-23 23:54:21 +0530

    Update __init__.py

--- promptify/__init__.py
+++ promptify/__init__.py
@@ -1,6 +1,9 @@
 __version__ = "0.1.4"
 from .parser.parser import Parser
 from .prompter.nlp_prompter import Prompter
+from .prompter.prompt_cache import PromptCache
+from .prompter.template_loader import TemplateLoader
+from .prompter.conversation_logger import ConversationLogger
 from .models.text2text.api.openai_complete import OpenAI
 from .models.text2text.api.hub_model import HubModel
 from .models.text2text.api.mock_model import MockModel
