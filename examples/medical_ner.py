a121b88c87b7b712552287a6252b2103a60ff90b
Monk <aadityaura@gmail.com>
2023-07-22 09:52:39 +0530

    Update medical_ner.py

--- examples/medical_ner.py
+++ examples/medical_ner.py
@@ -1,18 +1,15 @@
-from promptify import OpenAI
-from promptify import Prompter
+from promptify import Prompter,OpenAI, Pipeline
 
-sentence = """The patient is a 93-year-old female with a medical  				 
+sentence     =  "The patient is a 93-year-old female with a medical  				 
                 history of chronic right hip pain, osteoporosis,					
                 hypertension, depression, and chronic atrial						
                 fibrillation admitted for evaluation and management				
                 of severe nausea and vomiting and urinary tract				
-                infection"""
+                infection"
 
+model        = OpenAI(api_key) # or `HubModel()` for Huggingface-based inference or 'Azure' etc
+prompter     = Prompter('ner.jinja') # select a template or provide custom template
+pipe         = Pipeline(prompter , model)
 
-model = OpenAI(api_key="")
-
-prompter = Prompter(model=model, template="ner.jinja")
-
-output = prompter.fit(text_input=sentence, domain="medical", labels=None)
-
+output = pipe.fit(text_input=sentence, domain="medical", labels=None)
 print(output)
