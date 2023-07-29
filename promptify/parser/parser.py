aaf8d96b586c8b10603c378639f8014c7244b780
Monk <aadityaura@gmail.com>
2023-07-29 15:44:24 +0530

    Update parser.py

--- promptify/parser/parser.py
+++ promptify/parser/parser.py
@@ -162,7 +162,7 @@ class Parser:
             return python_obj
 
     def get_possible_completions(
-        self, json_str: str, max_completion_length: int = 5
+        self, json_str: str, json_depth_limit: int = 5
     ) -> Union[Dict[str, Any], List[Any]]:
         """
         Returns a list of possible completions for a JSON object string.
@@ -171,7 +171,7 @@ class Parser:
         ----------
         json_str : str
             The JSON object string
-        max_completion_length : int, optional
+        json_depth_limit : int, optional
             The maximum length of the completion strings to try (default is 5)
 
         Returns
@@ -190,7 +190,7 @@ class Parser:
         should_end_mark = "]" if json_str.strip()[0] == "[" else "}"
         completions = []
         for completion_str in self.get_combinations(
-            candidate_marks, max_completion_length, should_end_mark=should_end_mark
+            candidate_marks, json_depth_limit, should_end_mark=should_end_mark
         ):
             try:
                 completed_obj = self.complete_json_object(json_str, completion_str)
@@ -199,7 +199,7 @@ class Parser:
                 pass
         return self.find_max_length(completions)
 
-    def fit(self, json_str: str, max_completion_length: int = 5) -> Dict[str, Any]:
+    def fit(self, json_str: str, json_depth_limit: int = 5) -> Dict[str, Any]:
         """
         Tries to parse the input JSON string and complete it if it is incomplete.
 
@@ -207,7 +207,7 @@ class Parser:
         ----------
         json_str : str
             The input JSON string
-        max_completion_length : int, optional
+        json_depth_limit : int, optional
             The maximum length of the completion strings to try (default is 5)
 
         Returns
@@ -230,7 +230,7 @@ class Parser:
             json_str = re.sub(r"[\[\]\{\}\s]+$", "", json_str)
             try:
                 output = self.get_possible_completions(
-                    json_str, max_completion_length=max_completion_length
+                    json_str, json_depth_limit=json_depth_limit
                 )
                 return {
                     "status": "completed",
